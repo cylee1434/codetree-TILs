2439be98dd8a02a9a887fa5554eef7c2b74aa5a0
cylee1434 <86937368+cylee1434@users.noreply.github.com>
2024-07-20 18:11:44 +0900

    Title: 갈 수 있는 곳들, Time: 217ms, Memory: 26MB, Status: Passed - CodeTree

--- 240720/갈 수 있는 곳들/places-can-go.py
+++ 240720/갈 수 있는 곳들/places-can-go.py
@@ -1,40 +1,41 @@
 from collections import deque
 
-n, k = map(int, input().split())
+n, k = tuple(map(int, input().split()))
 
-# 입력 받은 격자 정보
-grid = [list(map(int, input().split())) for _ in range(n)]
+grid = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
 
-# 시작점 리스트
-start_points = [tuple(map(int, input().split())) for _ in range(k)]
+v = [
+    [False for _ in range(n)]
+    for _ in range(n)
+]
 
-# 방문 여부를 저장할 2차원 리스트
-visited = [[False] * n for _ in range(n)]
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
 
-# 방향 벡터 (상하좌우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+q = deque()
 
-def bfs(start_points):
-    q = deque(start_points)
-    for x, y in start_points:
-        visited[x][y] = True
+def bfs():
     
     while q:
         x, y = q.popleft()
-        
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and grid[nx][ny] == 0:
-                visited[nx][ny] = True
-                q.append((nx, ny))
 
-# 시작점을 0 기반 인덱스로 변환하여 bfs 함수 호출
-start_points = [(r-1, c-1) for r, c in start_points]
-bfs(start_points)
+        for di, dj in zip(dx,dy):
+            nx, ny = x + di, y + dj
 
-# 방문 가능한 칸 수를 계산
-reachable_count = sum(sum(row) for row in visited)
+            if 0<= nx < n and 0 <= ny < n and grid[nx][ny] == 0 and not v[nx][ny]:
+                q.append((nx,ny))
+                v[nx][ny] = True
 
-print(reachable_count)
+for _ in range(k):
+    x, y = tuple(map(int, input().split()))
+    q.append((x-1, y-1))
+    v[x-1][y-1] = True
+
+bfs()
+
+ans = sum([1 for i in range(n) for j in range(n) if v[i][j]])
+
+print(ans)
