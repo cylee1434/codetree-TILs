70dfcd6b37f481733ad958f9f7027671e96d6994
cylee1434 <86937368+cylee1434@users.noreply.github.com>
2024-10-30 11:05:14 +0900

    Title: 코드트리 코딩 대회, Time: 146ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241030/코드트리 코딩 대회/codetree-coding-contest.py
+++ 241030/코드트리 코딩 대회/codetree-coding-contest.py
@@ -1,39 +1,41 @@
-from collections import defaultdict as dd
+from collections import defaultdict as dd 
 
 n, m = map(int, input().split())
 infos = list(map(int, input().split()))
 
-# 최대 집중력보다 큰 값이 infos에 있다면 -1 출력
+# 최대 집중력보다 크면x
 if max(infos) > m:
     print(-1)
 else:
-    answer = 13  # 초기 최소 인원 값
-    dp = set()   # 이미 방문한 상태 저장
+    # 누가 푸는지는 상관x
+    # 상태를 결정짓는 요소는 {집중1:남은, 집중력2:인원} 형태
+    answer = 13
+    dp = set()
 
     def dfs(focus, infos):
         global answer
 
-        # 이미 방문한 상태면 탈출
+        # 탈출 조건
         if tuple(focus.items()) in dp:
             return
         dp.add(tuple(focus.items()))
 
-        # 문제를 모두 풀었다면 최소 인원 갱신
+        # 다 풀었으면 최소 인원 갱신
         if not infos:
-            answer = min(answer, n - focus[m])
+            answer = min(answer, n-focus[m])
             return
-
-        # 백트래킹으로 탐색
+        # 백트래킹 형태로
         x = infos.pop()
         for f in list(focus.keys()):
-            if focus[f] and f >= x:  # 해당 집중력으로 문제를 풀 수 있는 경우
-                focus[f - x] += 1  # 집중력 감소
+            if focus[f] and f >= x: # 문제 하나 풀고
+                focus[f-x] += 1 # 집중력 f를 가진 사람 -> f-x로
                 focus[f] -= 1
-                dfs(focus, infos[:])  # infos 복사본 사용
-                focus[f - x] -= 1
+                if tuple(focus.items()) not in dp:
+                    dfs(focus, infos)
+                focus[f-x] -= 1
                 focus[f] += 1
         infos.append(x)
-
+    
     focus = dd(int)
     focus[m] = n
     dfs(focus, infos)
