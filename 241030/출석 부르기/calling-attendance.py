a1cda8a1569ebb1ddbea562208ceaa7661c99e3f
cylee1434 <86937368+cylee1434@users.noreply.github.com>
2024-10-30 15:03:03 +0900

    Title: 출석 부르기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241030/출석 부르기/calling-attendance.py
@@ -0,0 +1,12 @@
+# 출석번호 입력받기
+number = int(input())
+
+# 번호에 따른 이름 출력
+if number == 1:
+    print("John")
+elif number == 2:
+    print("Tom")
+elif number == 3:
+    print("Paul")
+else:
+    print("Vacancy")
