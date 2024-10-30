29144c8579c39abca9e62eec61395971a7b58b31
cylee1434 <86937368+cylee1434@users.noreply.github.com>
2024-10-30 15:13:55 +0900

    Title: 최대 2번의 연산, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241030/최대 2번의 연산/up-to-2-calculations.py
+++ 241030/최대 2번의 연산/up-to-2-calculations.py
@@ -4,6 +4,6 @@ a = int(input())
 a = a // 2 if a % 2 == 0 else (a + 1) // 2
 
 # 두 번째 과정
-result = a // 2 if a % 2 == 0 else (a + 1) // 2
+a = a // 2 if a % 2 == 0 else (a + 1) // 2
 
-print(result)
+print(a)
