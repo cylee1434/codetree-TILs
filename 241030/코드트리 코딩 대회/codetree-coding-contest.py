08e0ae077c90480d88847bd195457b2f090a13b5
cylee1434 <86937368+cylee1434@users.noreply.github.com>
2024-10-30 10:58:56 +0900

    Title: 코드트리 코딩 대회, Time: 151ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241030/코드트리 코딩 대회/codetree-coding-contest.py
+++ 241030/코드트리 코딩 대회/codetree-coding-contest.py
@@ -1,27 +1,40 @@
-def min_team_members(N, M, difficulties):
-    # DP 배열을 초기화합니다. 매우 큰 값으로 설정하여 최소값을 찾습니다.
-    dp = [float('inf')] * (1 << N)
-    dp[0] = 0  # 아무 문제도 풀지 않은 상태는 팀원이 필요하지 않음
+from collections import defaultdict as dd
 
-    # 모든 상태를 탐색하며 dp를 업데이트합니다.
-    for mask in range(1 << N):
-        if dp[mask] == float('inf'):
-            continue
-        # 팀원이 풀 수 있는 문제들의 부분 집합을 구합니다.
-        for i in range(1 << N):
-            subset_sum = 0
-            for j in range(N):
-                if i & (1 << j) and not (mask & (1 << j)):
-                    subset_sum += difficulties[j]
-            if subset_sum <= M:
-                dp[mask | i] = min(dp[mask | i], dp[mask] + 1)
+n, m = map(int, input().split())
+infos = list(map(int, input().split()))
 
-    # 모든 문제를 풀기 위한 최소 팀원 수를 반환
-    return dp[(1 << N) - 1] if dp[(1 << N) - 1] != float('inf') else -1
+# 최대 집중력보다 큰 값이 infos에 있다면 -1 출력
+if max(infos) > m:
+    print(-1)
+else:
+    answer = 13  # 초기 최소 인원 값
+    dp = set()   # 이미 방문한 상태 저장
 
-# 사용자 입력 받기
-N, M = map(int, input().split())
-difficulties = list(map(int, input().split()))
+    def dfs(focus, infos):
+        global answer
 
-# 결과 출력
-print(min_team_members(N, M, difficulties))
+        # 이미 방문한 상태면 탈출
+        if tuple(focus.items()) in dp:
+            return
+        dp.add(tuple(focus.items()))
+
+        # 문제를 모두 풀었다면 최소 인원 갱신
+        if not infos:
+            answer = min(answer, n - focus[m])
+            return
+
+        # 백트래킹으로 탐색
+        x = infos.pop()
+        for f in list(focus.keys()):
+            if focus[f] and f >= x:  # 해당 집중력으로 문제를 풀 수 있는 경우
+                focus[f - x] += 1  # 집중력 감소
+                focus[f] -= 1
+                dfs(focus, infos[:])  # infos 복사본 사용
+                focus[f - x] -= 1
+                focus[f] += 1
+        infos.append(x)
+
+    focus = dd(int)
+    focus[m] = n
+    dfs(focus, infos)
+    print(answer)
