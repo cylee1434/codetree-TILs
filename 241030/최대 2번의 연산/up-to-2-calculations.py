b233a7d0d7faf750183eb8cedfe611d57811cd8e
cylee1434 <86937368+cylee1434@users.noreply.github.com>
2024-10-30 15:14:21 +0900

    Title: 최대 2번의 연산, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241030/최대 2번의 연산/up-to-2-calculations.py
+++ 241030/최대 2번의 연산/up-to-2-calculations.py
@@ -1,9 +1,13 @@
 a = int(input())
 
-# 첫 번째 과정
-a = a // 2 if a % 2 == 0 else (a + 1) // 2
+# 첫 번째 과정: 짝수인지 확인
+if a % 2 == 0:
+    a = a // 2
+else:
+    a = (a + 1) // 2
 
-# 두 번째 과정
-a = a // 2 if a % 2 == 0 else (a + 1) // 2
-
-print(a)
+# 두 번째 과정: 최종 조건 확인
+if a % 2 == 0:
+    print(a)
+else:
+    print((a + 1) // 2)
