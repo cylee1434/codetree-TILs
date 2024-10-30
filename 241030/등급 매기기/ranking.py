314ee7dd50488457d62cd8d0dbaa3fbeb9bedb8d
cylee1434 <86937368+cylee1434@users.noreply.github.com>
2024-10-30 15:04:32 +0900

    Title: 등급 매기기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241030/등급 매기기/ranking.py
@@ -0,0 +1,14 @@
+# 시험 점수 입력받기
+n = int(input())
+
+# 조건에 따른 등급 출력
+if n >= 90:
+    print("A")
+elif n >= 80:
+    print("B")
+elif n >= 70:
+    print("C")
+elif n >= 60:
+    print("D")
+else:
+    print("F")
