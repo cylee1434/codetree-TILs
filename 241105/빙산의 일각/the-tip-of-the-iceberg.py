e18cb77b10b6f90e1488e506a645d1691e45737a
cylee1434 <86937368+cylee1434@users.noreply.github.com>
2024-11-05 14:58:03 +0900

    Title: 빙산의 일각, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/빙산의 일각/the-tip-of-the-iceberg.py
+++ 241105/빙산의 일각/the-tip-of-the-iceberg.py
@@ -1,11 +1,4 @@
 import sys
-from operator import attrgetter
-
-# 빙산 구조체 정의
-class Iceberg:
-    def __init__(self, index, height):
-        self.index = index
-        self.height = height
 
 # 최대값 정의
 MAX_N = 100000 + 5
@@ -17,24 +10,25 @@ is_underwater = [False] * MAX_N
 # 빙산의 개수 입력
 num_icebergs = int(input())
 
-# 빙산의 높이와 인덱스를 입력받아 배열에 저장
+# 빙산의 높이와 인덱스를 입력받아 튜플로 저장
 for i in range(num_icebergs):
     height = int(input())
-    icebergs.append(Iceberg(i, height))
+    icebergs.append((i, height))  # (index, height) 형태의 튜플로 저장
 
-# 빙산 배열을 높이를 기준으로 정렬
-icebergs.sort(key=attrgetter('height'))
+# 빙산 배열을 높이를 기준으로 정렬 (height 기준으로 정렬)
+icebergs.sort(key=lambda x: x[1])
 
-# 현재 빙산 덩어리의 개수와 최대 빙산 덩어리의 개수를 초기화
+# 현재 덩어리의 개수와 최대 덩어리 개수를 초기화
 current_islands = 1
 max_islands = 1
 
 # 모든 빙산에 대해 반복
 for i in range(num_icebergs):
-    idx = icebergs[i].index
+    idx = icebergs[i][0]
 
     # 현재 빙산을 물에 잠긴 것으로 표시
     is_underwater[idx] = True
+
     # 왼쪽과 오른쪽에 이웃한 빙산이 있는지 확인
     has_left_neighbor = idx > 0 and not is_underwater[idx - 1]
     has_right_neighbor = idx < num_icebergs - 1 and not is_underwater[idx + 1]
@@ -47,7 +41,7 @@ for i in range(num_icebergs):
         current_islands -= 1
 
     # 현재 높이에서의 빙산 덩어리 개수를 최대값과 비교하여 갱신
-    if (i == num_icebergs - 1 or icebergs[i + 1].height > icebergs[i].height) and current_islands > max_islands:
+    if (i == num_icebergs - 1 or icebergs[i + 1][1] > icebergs[i][1]) and current_islands > max_islands:
         max_islands = current_islands
 
 # 최대 빙산 덩어리 개수를 출력
